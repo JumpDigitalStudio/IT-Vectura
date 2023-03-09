d7f9b13f74f222b143fdeb5b5158bf241f7528e1
Илья Кузнецов <jumpdigital.studio@gmail.com>
2023-03-09 17:37:11 +0500

    V6.1 Anime.js

---
+++ assets/js/animations.js
@@ -0,0 +1,69 @@
+const block = document.querySelector(".lang-modal");
+const list = block.querySelector(".lang-modal__all");
+
+const dropDownFadeIn = () => {
+  const listHeight = list.getBoundingClientRect().height;
+
+  anime({
+    targets: block,
+    height: listHeight + 20,
+    easing: "easeInOutQuad",
+    // easing: "spring(1, 80, 513 0)",
+    // easing: "cubicBezier(.5, .05, .1, .3)",
+    duration: 300,
+  });
+
+  anime({
+    targets: list,
+    opacity: 1,
+    easing: "easeInOutQuad",
+    // easing: "spring(1, 80, 13, 0)",
+    // easing: "cubicBezier(.5, .05, .1, .3)",
+    duration: 300,
+    delay: 100,
+  });
+
+  anime({
+    targets: list.querySelectorAll(".lang-modal__item"),
+    translateX: [50, 0],
+    opacity: [0, 1],
+    easing: "easeInOutQuad",
+    // easing: "spring(1, 80, 13, 0)",
+    // easing: "cubicBezier(.5, .05, .1, .3)",
+    duration: 300,
+    delay: anime.stagger(50, { start: 150 }),
+  });
+};
+
+const dropDownFadeOut = () => {
+  anime({
+    targets: list.querySelectorAll(".lang-modal__item"),
+    opacity: [1, 0],
+    easing: "easeInOutQuad",
+    // easing: "spring(1, 80, 513 0)",
+    // easing: "cubicBezier(.5, .05, .1, .3)",
+    duration: 300,
+    delay: anime.stagger(50),
+  });
+
+  anime({
+    targets: list,
+    opacity: 0,
+    easing: "easeInOutQuad",
+    // easing: "spring(1, 80, 13, 0)",
+    // easing: "cubicBezier(.5, .05, .1, .3)",
+    duration: 300,
+  });
+
+  anime({
+    targets: block,
+    height: 20,
+    easing: "easeInOutQuad",
+    // easing: "spring(1, 80, 13, 0)",
+    // easing: "cubicBezier(.5, .05, .1, .3)",
+    duration: 300,
+  });
+};
+
+block.addEventListener("mouseenter", dropDownFadeIn);
+block.addEventListener("mouseleave", dropDownFadeOut);
